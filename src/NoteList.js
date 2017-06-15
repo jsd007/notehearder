7f4a7903498abd8204a5c0daea12f3a23b416656
James Dombro <jsd007@me.com>
2017-06-15 15:46:34 -0400

    update

--- src/NoteList.js
+++ src/NoteList.js
@@ -1,26 +1,18 @@
 import React from 'react'
 
 import './NoteList.css'
+import Note from './Note'
 
-const NoteList = () => {
+const NoteList = ({ notes }) => {
   return (
     <div className="NoteList">
-        <h3>Notes</h3>
-        <ul id="notes">
-          <li>
-            <div className="note">
-              <div className="note-title">
-                Citizens of distant epochs
-              </div>
-              <div className="note-body">
-                <p>
-                  Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?
-                </p>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
+      <h3>Notes</h3>
+      <ul id="notes">
+        {Object.keys(notes).map((noteId) => {
+          return <Note note={notes[noteId]} key={noteId} />
+        })}
+      </ul>
+    </div>
   )
 }
 
